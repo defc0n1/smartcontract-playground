32050fa4df74a06c3effc0e41e5772ea454bad2c
Thomas Bocek <tom@tomp2p.net>
2016-05-19 11:48:05 +0200

    added first testcase using the ethereum go simulator

---
+++ contract_test.go
@@ -0,0 +1,52 @@
+//install abigen as described here: https://github.com/ethereum/go-ethereum/wiki/Native-DApps:-Go-bindings-to-Ethereum-contracts
+//go:generate abigen --sol TemperatureMeasurementA.sol --pkg main --out TemperatureMeasurementA.go
+//go:generate abigen --sol TemperatureMeasurementB.sol --pkg main --out TemperatureMeasurementB.go
+
+package main
+
+import (
+	"flag"
+	"log"
+	"math/big"
+	"os"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+var tempContract *TemperatureMeasurementA
+var temperatureWriterAddress common.Address
+
+func TestMain(m *testing.M) {
+
+	keyTempWriter, _ := crypto.GenerateKey()
+	authTempWriter := bind.NewKeyedTransactor(keyTempWriter)
+
+	key, _ := crypto.GenerateKey()
+	auth := bind.NewKeyedTransactor(key)
+	sim := backends.NewSimulatedBackend(core.GenesisAccount{Address: auth.From, Balance: big.NewInt(10000000000)})
+	// Deploy a temp contract on the simulated blockchain
+	temperatureWriterAddress = authTempWriter.From
+	_, _, temp, err := DeployTemperatureMeasurementA(auth, sim, temperatureWriterAddress, uint8(0), uint8(30), uint16(10), "SBT")
+	if err != nil {
+		log.Printf("Failed to deploy new temp contract: %v", err)
+	}
+	// Print the current (non existent) and pending name of the contract
+
+	sim.Commit()
+	tempContract = temp
+	flag.Parse()
+	os.Exit(m.Run())
+}
+
+func TestFreshContractNotFailed(t *testing.T) {
+	name1, _ := tempContract.Failed(nil)
+	if name1 {
+		t.Log("fresh contract cannot be failed!")
+		t.Fail()
+	}
+}
