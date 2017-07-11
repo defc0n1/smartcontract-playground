cee4700b6b4112420737be9b1a38fd8829aa24d2
Thomas Bocek <tom@tomp2p.net>
2017-07-11 11:05:28 +0200

    improvment and played with bonus

--- ModumToken.sol
+++ ModumToken.sol
@@ -32,8 +32,14 @@ contract ERC20Interface {
 contract ModumToken is ERC20Interface {
     
     address owner;
-    mapping (address => uint) accounts;
+    mapping (address => Account) accounts;
     mapping(address => mapping (address => uint256)) allowed;
+    struct Account {
+		uint bonusWei;
+		uint valueMod;
+		uint8 active;
+    }
+    address[] internal bonusMembers;
     
     uint unlockedTokens;
     uint lockedTokens = 10 * 1000 * 1000;
@@ -41,6 +47,7 @@ contract ModumToken is ERC20Interface {
     //https://www.epochconverter.com -> 1.9.2017 + 1 week
     uint mintPhaseEnd = 1504224000 + 1 weeks; //1 week after ICO, to be defined
     uint votingDuration = 2 weeks;
+    uint8 quorumWeight = 200; //100 -> is 1, 150 is 1.5, with 200 you need 2/3 of the votes
     
     string public constant name = "Modum Token";
     string public constant symbol = "MOD";
@@ -48,49 +55,48 @@ contract ModumToken is ERC20Interface {
     struct Proposal {
         string addr;
         bytes32 hash;
-        uint value; //proposal with 0 amount is invalid
+        uint valueMod; //proposal with 0 amount is invalid
         uint startTime;
         uint yay;
         uint nay;
-        uint nrVotes;
     }
     
     Proposal currentProposal;
 
     function ModumToken() {
         owner = msg.sender;
+        //we are not a bonus member, so don't add msg.sender to bonusMembers
+        accounts[msg.sender] = Account(0,0,1);
     }
     
     function proposal(string _addr, bytes32 _hash, uint _value) returns (bool) {
         //only if no proposal is pending
-        if(currentProposal.value > 0) {
-            return false;
-        }
+        require(currentProposal.valueMod == 0);
         uint _yay = unlockedTokens; // default is yes
         uint _nay = 0;
-        currentProposal = Proposal(_addr, _hash, _value, now, _yay, _nay, 0);
+        currentProposal = Proposal(_addr, _hash, _value, now, _yay, _nay);
         return true;
     }
     
     function vote(bool _vote) returns (uint) {
-        uint votingPower = accounts[msg.sender];
+        uint votingPower = accounts[msg.sender].valueMod;
         if(! _vote) {
             currentProposal.yay -= votingPower;
             currentProposal.nay += votingPower;
         }
-        currentProposal.nrVotes++;
         return votingPower;
     }
     
-    function collect() returns (bool) {
-        if(msg.sender == owner &&
-                currentProposal.value > 0 &&
+    function claimProposal() returns (bool) {
+        require(msg.sender == owner);
+        if(currentProposal.valueMod > 0 &&
                 now > currentProposal.startTime + votingDuration &&
-                currentProposal.yay > currentProposal.nay &&
-                lockedTokens - currentProposal.value >=0) {
-            accounts[msg.sender] += currentProposal.value;
-            unlockedTokens += currentProposal.value;
-            lockedTokens -= currentProposal.value;
+                lockedTokens >= currentProposal.valueMod) {
+            if(currentProposal.yay > (currentProposal.nay * quorumWeight) / 100) {
+                accounts[msg.sender].valueMod += currentProposal.valueMod;
+                unlockedTokens += currentProposal.valueMod;
+                lockedTokens -= currentProposal.valueMod;
+            }
             delete currentProposal;
             return true;
         }
@@ -98,44 +104,82 @@ contract ModumToken is ERC20Interface {
     }
     
     function mint(address _recipient, uint _value) returns (bool) {
-        if(msg.sender == owner && 
-                (now < mintPhaseEnd) &&
+        require(msg.sender == owner);
+        if(now < mintPhaseEnd &&
                 (lockedTokens + unlockedTokens + _value) <= maxTokens) {
-            accounts[_recipient] += _value;
+            if(accounts[_recipient].active == 0) {
+                accounts[_recipient].active = 1;
+                bonusMembers.push(_recipient);
+            }
+            accounts[_recipient].valueMod += _value;
             unlockedTokens += _value;
+            
             return true;
         }
         return false;
+    }
+    
+    function bonus() payable returns (bool) {
+        require(msg.sender == owner);
+        //distribute the wei to the token holders
+        uint totalBonus;
+        //don't add our tokens, as we don't get any bonus
+        uint tokenCount = unlockedTokens - accounts[msg.sender].valueMod;
+        for (uint i = 0; i < bonusMembers.length; i++) {
+            uint bonus = (msg.value * accounts[bonusMembers[i]].valueMod) / tokenCount;
+            accounts[bonusMembers[i]].bonusWei += bonus;
+            totalBonus += bonus;
+        }
+        //rounding goes to us
+        accounts[msg.sender].bonusWei += msg.value - totalBonus;
+    }
+    
+    function claimBonus() returns (bool) {
+        msg.sender.transfer(accounts[msg.sender].bonusWei);
+        accounts[msg.sender].bonusWei = 0;
+        return true;
     }
     
     function totalSupply() constant returns (uint) {
         return unlockedTokens + lockedTokens;
     }
     function balanceOf(address _owner) constant returns (uint balance) {
-        return accounts[_owner];
+        return accounts[_owner].valueMod;
     }
     function transfer(address _to, uint _value) returns (bool success) {
-        if (accounts[msg.sender] >= _value 
+        if (accounts[msg.sender].valueMod >= _value 
             && _value > 0
-            && accounts[_to] + _value > accounts[_to]) {
-            accounts[msg.sender] -= _value;
-            accounts[_to] += _value;
-            Transfer(msg.sender, _to, _value);
-            return true;
+            && accounts[_to].valueMod + _value > accounts[_to].valueMod) {
+                
+                if(accounts[_to].active == 0) {
+                    accounts[_to].active = 1;
+                    bonusMembers.push(_to);
+                }
+                
+                accounts[msg.sender].valueMod -= _value;
+                accounts[_to].valueMod += _value;
+                Transfer(msg.sender, _to, _value);
+                return true;
         } else {
             return false;
         }
     }
     
     function transferFrom(address _from, address _to, uint _value) returns (bool success) {
-        if (accounts[_from] >= _value
+        if (accounts[_from].valueMod >= _value
             && allowed[_from][msg.sender] >= _value
             && _value > 0
-            && accounts[_to] + _value > accounts[_to]) {
-            accounts[_from] -= _value;
-            allowed[_from][msg.sender] -= _value;
-            accounts[_to] += _value;
-            return true;
+            && accounts[_to].valueMod + _value > accounts[_to].valueMod) {
+                
+                if(accounts[_to].active == 0) {
+                    accounts[_to].active = 1;
+                    bonusMembers.push(_to);
+                }
+                
+                accounts[_from].valueMod -= _value;
+                allowed[_from][msg.sender] -= _value;
+                accounts[_to].valueMod += _value;
+                return true;
         } else {
             return false;
         }
@@ -153,4 +197,3 @@ contract ModumToken is ERC20Interface {
 
     event Approval(address indexed _owner, address indexed _spender, uint _value);
 }
-
